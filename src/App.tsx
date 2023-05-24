7f190f39764ebf3796cdc99135b0626e2b14a42f
Rolando Rio <rolandorio06@gmail.com>
2023-05-24 17:36:30 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <button className='btn' onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+            </button>
+        </>
+    );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+
+  // daisyUI config
+  daisyui: {
+    styled: true,
+    themes: ['winter'],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
+}
+
